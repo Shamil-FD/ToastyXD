56ca2a3f4c7833677092f98f36df38f232e655c5
FC5570 <68158483+FC5570@users.noreply.github.com>
2021-04-02 21:04:51 +0530

    Forgot exampleConfig

---
+++ Util/exampleConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+	StaffReportChnl: '', // staff report channel, to log automod and other actions
+	StaffRole: '', // role that every staff user has
+	StaffManagerRole: '', // staff manager role
+	NotVerifiedRole: '', // role to give to not verified users
+	Mongo: '', // mongo uri
+	Token: '', // discord bot token
+	Github: 'https://github.com/Shamil-FD/ToastyXD', // do not change this!
+};
