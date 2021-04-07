44b024c0e8050e00f378a2cd603c266053d56119
Milo <50248166+Milo123459@users.noreply.github.com>
2021-04-07 10:10:49 +0100

    Update exec.js to allow mentions

--- Commands/Staff Management/Exec.js
+++ Commands/Staff Management/Exec.js
@@ -19,7 +19,7 @@ module.exports = class ExecCommand extends Command {
 			return message.send(message.author, {
 				embeds: {
 					description:
-						'Proper Usage: t)exec @User t)CommandName <ARGS>\nExample: t)exec @User channelmute 5m He sucks',
+						'Proper Usage: t)exec @User t)CommandName <ARGS>\nExample: t)exec @User channelmute @User 5m He sucks',
 				},
 			});
 
@@ -29,6 +29,7 @@ module.exports = class ExecCommand extends Command {
 		message.react(this.client.tick);
 		message.author = user.user;
 		message.content = content;
-		await this.client.emit('message', message);
+        message.mentions.members = message.mentions.members.delete(message.mentions.members.first().id);
+		this.client.emit('message', message);
 	}
 };
