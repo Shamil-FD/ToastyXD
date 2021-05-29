42e75d19bab4ad34a0ce62072e3530e0a7bdb9cd
Milo <50248166+Milo123459@users.noreply.github.com>
2021-05-29 18:04:47 +0100

    wow such coolness debug event

---
+++ src/Listeners/Debug.js
@@ -0,0 +1,14 @@
+const { Listener } = require('discord-akairo');
+
+module.exports = class DebugListener extends Listener {
+	constructor() {
+		super('debug', {
+			emitter: 'client',
+			event: 'debug',
+		});
+	}
+
+	exec(info) {
+        console.log(info)
+	}
+};
