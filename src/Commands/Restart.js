123f2ac25a19fcc89e3199067698e14b04766dd4
Milo <50248166+Milo123459@users.noreply.github.com>
2021-06-13 16:50:04 +0100

    restart command

---
+++ src/Commands/Restart.js
@@ -0,0 +1,18 @@
+const Command = require('../Struct/Command.js');
+const util = require('util');
+
+module.exports = class RestartCommand extends Command {
+	constructor() {
+		super('restart', {
+			aliases: ['restart'],
+			category: 'flag',
+			ownerOnly: true,
+			quoted: false
+		});
+	}
+
+	async exec(message) {
+        message.react("✅");
+        require("child_process").execSync("git pull && pm2 reload Toasty")
+    }
+};
