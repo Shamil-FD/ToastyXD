30ca528e61d08ede0e252b56a0f4fb1f04feb40e
Milo <50248166+Milo123459@users.noreply.github.com>
2021-06-13 17:42:23 +0100

    yarn on top

--- src/Commands/Staff Management/ForcePull.js
+++ src/Commands/Staff Management/ForcePull.js
@@ -12,7 +12,7 @@ module.exports = class ForcePullCommand extends Command {
 
 	async exec(message) {
 		exec(`git pull ${this.client.config.Github}`, () => {
-			exec('npm i', console.log);
+			exec('yarn', console.log);
 		});
 		return message.react(this.client.tick);
 	}
