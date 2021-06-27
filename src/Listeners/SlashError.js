0bc3d6c728d59bb6242032aff36b03efe206b6de
sshamilpp1@gmail.com <sshamilpp1@gmail.com>
2021-06-27 20:33:16 +0100

    Add channel to report errors

--- src/Listeners/SlashError.js
+++ src/Listeners/SlashError.js
@@ -12,5 +12,6 @@ module.exports = class ErrorListener extends Listener {
     if (message.replied) message.editReply(`There was an error.\n\`\`\`${e.stack}\`\`\``);
     else message.reply(`There was an error.\n\`\`\`${e.stack}\`\`\``);
     console.log(e);
+    return this.client.channels.cache.get('850627411698647050').send({ content: `Error on ${command}\n\`\`\`${e.stack}\`\`\``})
   }
 };
