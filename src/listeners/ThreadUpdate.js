ca63c7d7df26c7ac972088a7a7e86cee96b61ec8
shamil <sshamilpp1@gmail.com>
2021-12-07 13:28:42 -0600

    Add another if statement

--- src/listeners/ThreadUpdate.js
+++ src/listeners/ThreadUpdate.js
@@ -9,6 +9,7 @@ module.exports = class ThreadUpdateListener extends Listener {
   async run(Old, New) {
       if (Old.archived === New.archived) return;
       if (New.archived === true) return;
+      if (New.lastMessage && New.lastMessage.author.id === New.client.user.id && New.lastMessage.embeds[0].title.toLowerCase() === 'thread archived') return;
       
       await New.setArchived(false);
       await New.send({ embeds: [New.client.tools.embed().setDescription('This thread has been archived.').setColor('RED').setTitle('Archived')] });
